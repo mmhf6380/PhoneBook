53b02a4a8ed0d037a6bea870de225b906ca9509d
S.Mahdi Hoseini <mmhf6380@gmail.com>
2019-09-21 17:43:40 +0430

    Add Service Interface Contract

---
+++ Phonebook.Services.ApplicationServices/IPersonSevice.cs
@@ -0,0 +1,23 @@
+﻿using PhoneBook.Core.Entites.People;
+using PhoneBook.Core.Entites.Phones;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Phonebook.Services.ApplicationServices
+{
+    public interface IPersonSevice
+    {
+        List<Person> GetAllPerson();
+        Person GetPerson(int personId);
+        Person GetPersonWithPhoneList(int personId);
+        void AddPerson(Person person);
+        void AppPhoneForPerson(int personId, Phone phone);
+        void UpdatePerson(int personId);
+        void UpdatePhoneForPerson(int phoneId);
+        void DeletePerson(int personId);
+        void DeletePhoneForPerson(int PhoneId);
+
+
+    }
+}
